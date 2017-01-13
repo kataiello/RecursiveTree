2d063376c73341059ad6c82e41ff1ee39a4699f7
Kat Aiello <aiell22k@mtholyoke.edu>
2017-01-13 13:14:33 -0500

    basing design on mandelbrot app

--- app/src/main/java/com/example/demouser/recursivetree/MainActivity.java
+++ app/src/main/java/com/example/demouser/recursivetree/MainActivity.java
@@ -2,20 +2,60 @@ package com.example.demouser.recursivetree;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public double GOLDEN_RATIO = 1.618;
-    public double MIN_LENGTH = 3;
+    public double minLength = 3;
     public double MAX_ANGLE = 0.5 * Math.PI;
     public int width;
     public int height;
+    private int mDepth = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TreeView treeView = (TreeView) findViewById(R.id.tree_view);
+        final TextView depthText = (TextView) findViewById(R.id.depth);
+        depthText.setText(String.valueOf(minLength));
+
+        Button resetBtn = (Button) findViewById(R.id.reset);
+
+        resetBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                resetFromDepth(treeView, depthText);
+            }
+        });
+
+        Button decreaseBtn = (Button) findViewById(R.id.decrease_depth);
+        decreaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (minLength > 0) {
+                    minLength--;
+                }
+                depthText.setText(String.valueOf(minLength));
+            }
+        });
+
+        Button increaseBtn = (Button) findViewById(R.id.increase_depth);
+        increaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                depthText.setText(String.valueOf(++minLength));
+            }
+        });
+
+        resetFromDepth(treeView, depthText);
     }
 
     public void drawBranch(int x, int y, double angle, double length)
@@ -27,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
          * y < 0, y > height
          * length < k, k = 3, 5?
          */
-        if(x > 0 && x < width && y > 0 && y < height && length > MIN_LENGTH)
+        if(x > 0 && x < width && y > 0 && y < height && length > minLength)
         {
 
             /*
@@ -55,15 +95,18 @@ public class MainActivity extends AppCompatActivity {
              * drawBranch(new_x, new_y, angle?, length)
              */
 
-            //calculate a random angle between [theta - delta, theta + delta]
+            //calculate a random angle between [theta - delta, theta] for the left
+            // and [theta, theta + delta] for the right
             //where theta is the angle of this branch and delta is MAX_ANGLE
-            double newAngle = (Math.random() - 0.5) * Math.PI + angle;
+            double newAngle1 = (Math.random()/(-2)) * Math.PI + angle;
+            double newAngle2 = (Math.random()/2) * Math.PI + angle;
 
             //calculate the new length using the golden ratio
             double newLength = length / GOLDEN_RATIO;
 
             //call recursion
-            drawBranch(newX, newY, newAngle, newLength);
+            drawBranch(newX, newY, newAngle1, newLength);
+            drawBranch(newX, newY, newAngle2, newLength);
 
         }
 
--- app/src/main/java/com/example/demouser/recursivetree/TreeView.java
+++ app/src/main/java/com/example/demouser/recursivetree/TreeView.java
@@ -4,5 +4,151 @@ package com.example.demouser.recursivetree;
  * Created by demouser on 1/13/17.
  */
 
-public class TreeView {
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class TreeView extends View {
+
+    private static final double X_MAX = 1;
+    private static final double X_MIN = -2.5;
+    private static final double Y_MAX = 1.75;
+    private static final double Y_MIN = -1.75;
+
+    private double mXMin = X_MIN;
+    private double mXMax = X_MAX;
+    private double mXRange = X_MAX - X_MIN;
+    private double mYMin = Y_MIN;
+    private double mYMax = Y_MAX;
+    private double mYRange = Y_MAX - Y_MIN;
+
+    private Paint mBackgroundPaint;
+    private Paint mPaint;
+    private int mMaxDepth;
+    private int mNumPoints = 100;
+
+    private int[] mColors;
+
+    public MandelbrotView(Context context) {
+        super(context);
+        init();
+    }
+
+    public MandelbrotView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MandelbrotView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public MandelbrotView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(getResources().getColor(R.color.background_color));
+
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        if (widthMeasureSpec == 0 || heightMeasureSpec == 0) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+        int size = Math.max(widthMeasureSpec, heightMeasureSpec);
+        super.onMeasure(size, size);
+    }
+
+    public void reset(int depth) {
+        mMaxDepth = depth;
+        mXMin = X_MIN;
+        mXMax = X_MAX;
+        mXRange = mXMax - mXMin;
+        mYMin = Y_MIN;
+        mYMax = Y_MAX;
+        mYRange = mYMax - mYMin;
+        initializeColors();
+        postInvalidate();
+    }
+
+    /**
+     * Define the colors to use, based on the depth.
+     * This will mean less calculation during onDraw!
+     */
+    private void initializeColors() {
+        mColors = new int[mMaxDepth + 1];
+        for (int i = 0; i < mMaxDepth; i++) {
+            int r = (int) (255 * (i * 1.0 / mMaxDepth));
+            int g = (int) (255 * (1 - i * 1.0 / mMaxDepth));
+            mColors[i] = Color.rgb(r, g, 255);
+        }
+        mColors[mMaxDepth] = Color.BLACK;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        canvas.drawRect(0, 0, getWidth(), getHeight(), mBackgroundPaint);
+
+        int dotSize = getWidth() / mNumPoints;
+
+        for(int i = 0; i < mNumPoints; i++)
+        {
+            double mx = ((i * 1.0) / mNumPoints) * mXRange + mXMin;
+            float px = ((i * 1.0f) / mNumPoints) * getWidth();
+
+            for(int j = 0; j < mNumPoints; j++)
+            {
+                double my = ((j * 1.0) / mNumPoints) * mYRange + mYMin;
+                float py = ((j * 1.0f) / mNumPoints) * getHeight();
+
+                int color = getMandelbrotColor(mx, my);
+                mPaint.setColor(color);
+                canvas.drawCircle(px, py, dotSize, mPaint);
+            }
+        }
+    }
+
+    /**
+     * Z_0 = (mX, mY)
+     * Z_n+1 = Z_n * Z_n + Z_0
+     * @param mX
+     * @param mY
+     * @return
+     */
+    private int getMandelbrotColor(double mX, double mY)
+    {
+        double zx = mX;
+        double zy = mY;
+        int iter = 0;
+
+        while(  iter < mMaxDepth &&
+                //check divergence
+                (zx*zx) + (zy * zy) < 4
+                )
+        {
+            double tx = (zx * zx) - (zy * zy) + mX;
+            zy = 2 * zx * zy + mY;
+            zx = tx;
+            iter++;
+        }
+
+        return mColors[iter];
+    }
 }
